1e8fb1e313cc93031c9e1e3eaa15e65be7956b1d
mohammadbinibrahim <109195335+mohammadbinibrahim@users.noreply.github.com>
2024-11-29 15:52:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
     foreach($blocked as $block){
         if (strpos(strtolower($result), $block) !== false){
-            echo "<p class='result'>ممنوع!</p>";
+            echo "<p class='result'>!ممنوع</p>";
             $cond = true;
             break;
         }
